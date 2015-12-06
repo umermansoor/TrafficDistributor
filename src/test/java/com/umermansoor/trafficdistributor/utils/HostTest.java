66d4e93bedfeef0b14ff23b12d3d678e00740ed3
Umer <umermk3@gmail.com>
2015-12-05 21:26:06 -0700

    Added tests for Host class.

--- src/test/java/com/umermansoor/trafficdistributor/utils/HostTest.java
+++ src/test/java/com/umermansoor/trafficdistributor/utils/HostTest.java
@@ -1,11 +1,16 @@
 package com.umermansoor.trafficdistributor.utils;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class HostTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testHost() {
@@ -25,10 +30,29 @@ public class HostTest {
 
         assertEquals("failure - equal instances are not equals.", h1, h2);
         assertNotEquals("failure - unequal instances are equals.", h1, h3);
+        assertNotEquals("failure - unequal instances are equals.", h1, new Object());
 
-
-
-
+        assertEquals("failure - hashcodes are not equal", h1.hashCode(), h2.hashCode());
     }
+
+    public void testInvalidCreation() throws NullPointerException, IllegalArgumentException {
+        thrown.expect(NullPointerException.class);
+        new Host(null, 1);
+
+        thrown.expect(IllegalArgumentException.class);
+        new Host("", 1);
+
+        thrown.expect(IllegalArgumentException.class);
+        String veryLongInvalidHostname = "Lorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, erat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia non2";
+        new Host(veryLongInvalidHostname, 1);
+
+        /** Invalid port numbers **/
+        thrown.expect(IllegalArgumentException.class);
+        new Host("localhost", -1);
+
+        thrown.expect(IllegalArgumentException.class);
+        new Host("localhost", 65536);
+    }
+
 
 }
